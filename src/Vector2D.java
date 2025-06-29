6bccd7a8e6a5bf674b92aaa363ef981585691433
yanis kerchou <yaniskerchou0@gmail.com>
2025-06-30 01:10:46 +0200

    Added Vector2D Class

--- src/Vector2D.java
+++ src/Vector2D.java
@@ -21,8 +21,8 @@ public class Vector2D {
     }
 
     public Vector2D() {
-        setX(0);
-        setY(0);
+        setX(0f);
+        setY(0f);
     }
 
     public Vector2D(float x, float y) {
