89252dfc1ac2ecd143c92dee6b2a8d6a76a621db
yanis kerchou <yaniskerchou0@gmail.com>
2025-06-30 01:07:11 +0200

    Added Vector2D Class

---
+++ src/Vector2D.java
@@ -0,0 +1,46 @@
+
+public class Vector2D {
+
+    private float m_x;
+    private float m_y;
+
+    public float getX() {
+        return m_x;
+    }
+
+    public void setX(float x) {
+        this.m_x = x;
+    }
+
+    public float getY() {
+        return this.m_y;
+    }
+
+    public void setY(float y) {
+        this.m_y = y;
+    }
+
+    public Vector2D() {
+        setX(0);
+        setY(0);
+    }
+
+    public Vector2D(float x, float y) {
+        setX(x);
+        setY(y);
+    }
+
+
+    public Vector2D add(Vector2D vector) {
+        m_x+= vector.getX();
+        m_y+= vector.getY();
+        return this ;
+    }
+
+    public Vector2D subtract(Vector2D vector) {
+        m_x-= vector.getX();
+        m_y-= vector.getY();
+        return this ;
+    }
+
+}
